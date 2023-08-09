f25ea4820851471c2f3f4cea3dcc4ee8fa8b466b
Azure_Moon <jordanw0412@gmail.com>
2023-08-08 23:36:51 -0400

    added added lightbox effect to videos and assigned each video an ID

---
+++ lightboxVid.js
@@ -0,0 +1,28 @@
+const lightbox = document.createElement('div')
+lightbox.id = 'lightbox'
+document.body.appendChild(lightbox)
+
+const videos = document.querySelectorAll('video')
+videos.forEach(video => {
+    video.addEventListener('click', e => {
+        lightbox.classList.add('active')
+        const vid = document.createElement('video')
+        vid.src = video.src 
+        while (lightbox.firstChild) {
+            lightbox.removeChild(lightbox.firstChild)
+        }
+        lightbox.appendChild(vid)
+    })
+})
+
+lightbox.addEventListener('click', e => {
+    if (e.target !== e.currentTarget) return
+    lightbox.classList.remove('active')
+})
+
+
+
+for(i = 0; i < videos.length; i++){
+    videos[i].id = i + 1
+}
+console.log(videos)
