d8074befdf33bb8fe52b698f1de4a3f719183d22
Azure_Moon <jordanw0412@gmail.com>
2023-08-08 21:26:38 -0400

    updated lightbox.js and designs.html to only affect desired images

--- lightbox.js
+++ lightbox.js
@@ -2,7 +2,7 @@ const lightbox = document.createElement('div')
 lightbox.id = 'lightbox'
 document.body.appendChild(lightbox)
 
-const images = document.querySelectorAll('img')
+const images = document.querySelectorAll('.img')
 images.forEach(image => {
     image.addEventListener('click', e => {
         lightbox.classList.add('active')
